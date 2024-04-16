398da0e47f0afd8c83620a5a08a92deca22960a0
Laye <tlayefaah@gmail.com>
2024-04-16 18:22:18 +0100

    build:hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,7 +7,7 @@ import Stats from "./ui/Stats";
 export default function Hero() {
   return (
     <section className=" hero_bg min-h-screen relative">
-      <div className="absolute top-[10%] -right-[55%] lg:top-[0%] lg:-right-[70%] 2xl:-right-[50%]">
+      <div className="absolute top-[10%] -right-[55%] lg:top-[0%] lg:-right-[70%] 2xl:-right-[50%] -z-0 ">
         <Image
           src="/images/big_glow.svg"
           height={500}
@@ -34,7 +34,7 @@ export default function Hero() {
             Dedicated to revolutionizing the way projects raise liquidity
             through presales.
           </p>
-          <div className="flex justify-center items-center gap-4 lg:gap-8 flex-col  lg:flex-row ">
+          <div className="flex justify-center items-center gap-4 lg:gap-8 flex-col  lg:flex-row z-10">
             <Button title="Launch app" css="w-full lg:w-[200px] " />
             <Button
               title="List my projects"
@@ -44,7 +44,7 @@ export default function Hero() {
           </div>
         </div>
 
-        <div className="bg-transparent flex flex-col sm:flex-row items-center justify-evenly h-[322px] sm:h-[162px] rounded-[40px] max-w-[280px] sm:max-w-[994px] gap-x-10 mx-auto w-full mt-20">
+        <div className="bg-transparent flex flex-col sm:flex-row items-center justify-evenly h-[322px] sm:h-[162px] rounded-[40px] max-w-[280px] sm:max-w-[994px] gap-x-10 mx-auto w-full mt-20 z-10">
           <Stats stat={532} title="Developers" />
           <Stats stat={532} title="Contributors" />
           <Stats stat={532} title="Projects" />
