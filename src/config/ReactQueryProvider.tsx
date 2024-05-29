72ae17c93f8c33e312310b118e97a84b34dc23ad
Tubolayefa <tlayefaah@gmail.com>
2024-05-29 10:45:54 +0100

    update

--- src/config/ReactQueryProvider.tsx
+++ src/config/ReactQueryProvider.tsx
@@ -7,7 +7,7 @@ const queryClient = new QueryClient({
     queries: {
       staleTime: 60000,
       gcTime: 10 * (60 * 1000),
-      // retry: 3,
+      retry: 1,
       refetchOnWindowFocus: false,
     },
   },
