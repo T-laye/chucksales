c34c26ae323ab8fd3fa2f081b0c1c8c55b6ac864
Tubolayefa <tlayefaah@gmail.com>
2024-05-31 14:10:58 +0100

    updated config

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -12,7 +12,7 @@ if (!projectId) throw new Error("Project ID is not defined");
 const metadata = {
   name: "Web3Modal",
   description: "Web3Modal Chucksales",
-  url: "https://web3modal.com", // origin must match your domain & subdomain
+  url: "https://www.chucksales.com", // origin must match your domain & subdomain
   icons: ["https://avatars.githubusercontent.com/u/37784886"],
 };
 
