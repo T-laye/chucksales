5f31f6992af7b424fed2b541134c5a1ce78db53c
Tubolayefa <tlayefaah@gmail.com>
2024-06-06 10:50:21 +0100

    push

--- src/config/Web3Provider.tsx
+++ src/config/Web3Provider.tsx
@@ -1,27 +1,42 @@
-'use client'
-import { WagmiProvider, createConfig, http } from "wagmi";
-import { mainnet } from "wagmi/chains";
+"use client";
+import { createConfig, Transport } from "@wagmi/core";
+// import { WagmiProvider, createConfig, http } from "wagmi";
+import { WagmiProvider, http } from "wagmi";
+import { base, mainnet, arbitrum, optimism } from "wagmi/chains";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// import { ConnectKitProvider, getDefaultConfig } from "connectkit";
 import { ConnectKitProvider, getDefaultConfig } from "connectkit";
 
-const config = createConfig(
+// import { mainnet, base, arbitrum, optimism } from "@wagmi/core/chains";
+// import { http } from "@wagmi/core/transports";
+
+// Define a compatible StorageItemMap
+interface StorageItemMap {
+  [key: string]: Transport; // Ensure it matches the expected Transport type
+}
+
+const config = createConfig<any, StorageItemMap>(
   getDefaultConfig({
     // Your dApps chains
-    chains: [mainnet],
+    chains: [mainnet, base, arbitrum, optimism],
     transports: {
       // RPC URL for each chain
       [mainnet.id]: http(),
+      [base.id]: http(),
+      [arbitrum.id]: http(),
+      [optimism.id]: http(),
     },
 
     // Required API Keys
     walletConnectProjectId: process.env.NEXT_PUBLIC_PROJECT_ID as any,
 
     // Required App Info
-    appName: "Your App Name",
+    appName: "Chucksale",
 
     // Optional App Info
-    appDescription: "Your App Description",
-    appUrl: "https://family.co", // your app's url
+    appDescription:
+      "Never struggle to raise liquidity. Dedicated to revolutionizing the way projects raise liquidity through presales.",
+    appUrl: "https://chucksale.vercel.app", // your app's url
     appIcon: "https://family.co/logo.png", // your app's icon, no bigger than 1024x1024px (max. 1MB)
   })
 );
@@ -30,7 +45,7 @@ const queryClient = new QueryClient();
 
 export const Web3Provider = ({ children }: any) => {
   return (
-    <WagmiProvider config={config}>
+    <WagmiProvider config={config as any}>
       <QueryClientProvider client={queryClient}>
         <ConnectKitProvider>{children}</ConnectKitProvider>
       </QueryClientProvider>
